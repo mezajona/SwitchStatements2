53204d11d8ee42f8d2613cbdbf50d9dddda1bb0c
Jonathan Meza <jonathansyear92@gmail.com>
2022-08-28 15:58:53 -0600

    Switch Statements part 2

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); // evaluate to a string
+
+           switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is an interesting subject!");
+                    break;
+                case "History":
+                    Console.WriteLine("History is an interesting subject!");
+                    break;
+                case "Chemistry":
+                    Console.WriteLine("Chemistry is an imprtant class!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
+        }
+    }
+}
+
